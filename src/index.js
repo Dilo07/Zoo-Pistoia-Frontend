08b5c8670d27722f12b0b56b43799f3d602373b3
Andrea Di Lorenzo <andrea.dilorenzo07@gmail.com>
2021-01-13 09:28:43 +0100

    13-1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import Dati from './zoo-pistoia-FrontEnd/home'
-/* import Avviso from './messaggio avviso' */
+import Dati from './zoo-pistoia/home'
+/* import Avviso from ./messaggio avviso' */
 
 
 class ZooPistoia extends React.Component{
